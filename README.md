b18e6b601ec5e37f44936461bb86be40572cd0c4
David Antillies <93983691+dongyan3721@users.noreply.github.com>
2024-03-04 13:46:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# https://github.com/dongyan3721/SoftwareCooperativeEngineering.git
---
+++ src/main/java/com/softwarecooperative/softwareciooperative/SoftwareciooperativeApplication.java
@@ -0,0 +1,13 @@
+package com.softwarecooperative.softwareciooperative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareciooperativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareciooperativeApplication.class, args);
+    }
+
+}
