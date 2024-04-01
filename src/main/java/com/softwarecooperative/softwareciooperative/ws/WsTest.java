6b46c58cea800d342d287b2cdc337af0168bc3c6
嘉然今天吃向晚 <1483823709@qq.com>
2024-04-02 00:39:31 +0800

    配置WebSocket

---
+++ src/main/java/com/softwarecooperative/softwareciooperative/ws/WsTest.java
@@ -0,0 +1,64 @@
+package com.softwarecooperative.softwareciooperative.ws;
+
+import jakarta.websocket.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.websocket.server.ServerEndpoint;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@ServerEndpoint("/test/{cid}")
+@Slf4j
+public class WsTest {
+
+    //存放会话对象
+    private static final Map<String, Session> sessionMap = new ConcurrentHashMap<>();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("cid") String cid) {
+        log.info("{}客户端登入", cid);
+        sessionMap.put(cid, session);
+    }
+
+    @OnMessage
+    public void onMessage(String message, @PathParam("cid") String cid) {
+        log.info("收到来自客户端：{}的信息: {}", cid, message);
+        sendToAllClient("我测你的码");
+    }
+
+    @OnClose
+    public void onClose(@PathParam("cid") String cid) throws IOException {
+        Session session = sessionMap.remove(cid);
+        session.close();
+        log.info("{}客户端连接断开", cid);
+    }
+    
+    @OnError
+    public void onError(@PathParam("cid") String cid, Throwable error) {
+        try {
+            Session session = sessionMap.remove(cid);
+            session.close();
+        } catch (Exception ignored) {}
+
+        log.info("傻逼{}客户端又出错", cid);
+    }
+
+    public void sendToAllClient(String message) {
+        Collection<Session> sessions = sessionMap.values();
+        for (Session session : sessions) {
+            try {
+                //服务器向客户端发送消息
+                session.getBasicRemote().sendText(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
